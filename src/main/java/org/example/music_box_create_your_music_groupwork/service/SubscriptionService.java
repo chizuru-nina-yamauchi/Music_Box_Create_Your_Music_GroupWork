cc2e78e2ff29bc55e5798027b1bc572812a3a749
Ioannis Laventzakis <glaventzakis@gmail.com>
2024-08-13 14:52:00 +0200

    Sub Service

--- src/main/java/org/example/music_box_create_your_music_groupwork/service/SubscriptionService.java
+++ src/main/java/org/example/music_box_create_your_music_groupwork/service/SubscriptionService.java
@@ -20,27 +20,12 @@ public class SubscriptionService {
     private final SubscriptionRepository subscriptionRepository;
     private final UserRepository userRepository;
 
-    /**
-     * Constructs a SubscriptionService with the specified repositories.
-     *
-     * @param subscriptionRepository the SubscriptionRepository to be used by this service
-     * @param userRepository the UserRepository to be used by this service
-     */
     @Autowired
     public SubscriptionService(SubscriptionRepository subscriptionRepository, UserRepository userRepository) {
         this.subscriptionRepository = subscriptionRepository;
         this.userRepository = userRepository;
     }
 
-    /**
-     * Creates a new subscription for a user.
-     *
-     * @param userId the ID of the user for whom the subscription is to be created
-     * @param startDate the start date of the subscription
-     * @param endDate the end date of the subscription
-     * @return the created Subscription
-     * @throws RuntimeException if the user is not found
-     */
     public Subscription createSubscription(Long userId, LocalDateTime startDate, LocalDateTime endDate) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -53,22 +38,10 @@ public class SubscriptionService {
         return subscriptionRepository.save(subscription);
     }
 
-    /**
-     * Retrieves a list of subscriptions for a user.
-     *
-     * @param userId the ID of the user whose subscriptions are to be retrieved
-     * @return a list of Subscriptions associated with the given user ID
-     */
     public List<Subscription> getSubscriptionsByUserId(Long userId) {
         return subscriptionRepository.findByUserId(userId);
     }
 
-    /**
-     * Cancels a subscription.
-     *
-     * @param subscriptionId the ID of the subscription to be canceled
-     * @throws RuntimeException if the subscription is not found
-     */
     public void cancelSubscription(Long subscriptionId) {
         Subscription subscription = subscriptionRepository.findById(subscriptionId)
                 .orElseThrow(() -> new RuntimeException("Subscription not found"));
@@ -76,5 +49,7 @@ public class SubscriptionService {
         subscriptionRepository.delete(subscription);
     }
 
-    // Additional methods can be added as needed
+    public boolean isUserSubscriber(Long userId) {
+        return !subscriptionRepository.findByUserId(userId).isEmpty();
+    }
 }
