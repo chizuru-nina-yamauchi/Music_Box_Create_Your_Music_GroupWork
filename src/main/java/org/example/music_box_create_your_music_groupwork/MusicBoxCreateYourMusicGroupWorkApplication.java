27b328670bdef52b41cc4e712c8c4e3b0e9d1f9c
Chizuru <chizuru.yamauchi@dci-student.org>
2024-08-13 10:13:06 +0200

    add maven and structure

---
+++ src/main/java/org/example/music_box_create_your_music_groupwork/MusicBoxCreateYourMusicGroupWorkApplication.java
@@ -0,0 +1,13 @@
+package org.example.music_box_create_your_music_groupwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBoxCreateYourMusicGroupWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicBoxCreateYourMusicGroupWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/music_box_create_your_music_groupwork/MusicBoxCreateYourMusicGroupWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.music_box_create_your_music_groupwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicBoxCreateYourMusicGroupWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
