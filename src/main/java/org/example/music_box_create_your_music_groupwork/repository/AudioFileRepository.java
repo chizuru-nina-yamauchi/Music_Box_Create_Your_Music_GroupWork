01ad4b1f44db39c5d086a5f96a8749b1bfe6ce14
Chizuru <chizuru.yamauchi@dci-student.org>
2024-08-13 13:13:10 +0200

    add Main App function and java files

---
+++ src/main/java/org/example/music_box_create_your_music_groupwork/repository/AudioFileRepository.java
@@ -0,0 +1,6 @@
+package org.example.music_box_create_your_music_groupwork.repository;
+
+
+// Not used for Main Music App part of the project, but to show the package, adding the java file
+public interface AudioFileRepository {
+}
