a2a59fbd08ba28fc9a6ae0b1429699e413674abc
Chizuru <chizuru.yamauchi@dci-student.org>
2024-08-14 15:31:15 +0200

    modify home.html, but need debugging

--- src/main/java/org/example/music_box_create_your_music_groupwork/config/WebConfig.java
+++ src/main/java/org/example/music_box_create_your_music_groupwork/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/audio/audio/**")
+        registry.addResourceHandler("/audio/**")
                 .addResourceLocations("classpath:/static/audio/");
     }
 
